e05eda5e165be20287c542956e45568b7d22aa2c
hasindusithmin <hasindusithmin64@gmail.com>
2023-04-23 17:38:30 +0530

    Add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '@/styles/globals.css'
-import '@splidejs/react-splide/css';
 import 'react-toastify/dist/ReactToastify.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
