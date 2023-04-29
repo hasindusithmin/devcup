5932d47666b73487b46b3f031760db0c71213632
hasindusithmin <hasindusithmin64@gmail.com>
2023-04-29 16:57:21 +0530

    PWA config

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="application-name" content="DevCup" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="DevCup" />
+        <meta name="description" content="Take a sip of coffee, and a gulp of developer wisdom" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#f2f2f2" />
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' href='/icon-192x192.png' />
+        <meta name='theme-color' content='#f2f2f2' />
+      </Head>
       <body>
         <Main />
         <NextScript />
